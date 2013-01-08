f1b786736181a0fa224718003bdba66756037ba1
cpf <cpf@cpfx.ca>
2013-01-08 18:48:31 -0500

    Wallclock snapping is complete, tweaked a few command-line args

--- README.md
+++ README.md
@@ -13,11 +13,17 @@ Instructions
 0. Max out webcam resolution (In Yawcam, Settings→Device xxxx→Device properties)
 0. Max out HTTP publish JPEG quality (In Yawcam, Settings→Edit Settings→Output/Http→Image quality)
 
+Scheduling
+----------
+yawstack has a built-in automatic capture feature: `--interval time_in_seconds`. The additional `--interval-snap` option will align captures to clean wallclock times. That is, with a 5 minute capture interval and `--interval-snap`, images would be captured at 12:35:00, 12:40:00, etc. instead of 12:33:15, 12:38:15, etc.
+
+*That said,* for ultimate long-term stability I recommend using cron/Task Scheduler to make sure logging isn't derailed by an undiscovered bug in this script. Unlikely, but possible.
 
 Usage
 -----
-	usage: yawstack.py [-h] [--archive PATH] [--image-url URL] [--interval SECS]
-	                   [--stack-depth COUNT] [--stack-interval SECS] [--verbose]
+	usage: yawstack.py [-h] [--output PATH] [--image-url URL] [--interval SECS]
+                   [--interval-snap] [--stack-depth COUNT]
+                   [--stack-interval SECS] [--verbose]
 
 	Yawcam companion script to automatically retrieve, noise-reduce, and archive
 	images. Best results if Yawcam is set to output full-quality JPEGs at maximum
@@ -25,11 +31,12 @@ Usage
 
 	optional arguments:
 	  -h, --help            show this help message and exit
-	  --archive PATH        Image archive path pattern (default
+	  --output PATH         Image output path pattern (default
 	                        archive/%H-%M-%S_%d-%m-%Y.jpg)
 	  --image-url URL       Webcam image URL (default
 	                        http://127.0.0.1:8888/out.jpg aka 'HTTP Output')
-	  --interval SECS       Archive interval (seconds, default 60)
+	  --interval SECS       Archive interval (seconds, default 0 = one-off)
+	  --interval-snap       Align captures with wall-clock times
 	  --stack-depth COUNT   Number of images to stack (default 10)
 	  --stack-interval SECS
 	                        Interval between consecutive images in stack (secs,
@@ -37,4 +44,3 @@ Usage
 	  --verbose             Dump lots of logging to stdout
 
 	IMPORTANT NOTE: ImageStack.exe must be in same directory as yawstack.py
-
--- yawstack.py
+++ yawstack.py
@@ -9,12 +9,12 @@
 ArchivePath = "archive/%H-%M-%S_%d-%m-%Y.jpg"
 # unless you change the HTTP publish settings, don't change this
 YawcamURL = "http://127.0.0.1:8888/out.jpg"
-# how often (secs) to capture and archive an image (time taken to stack images is accounted for)
-CaptureInterval = 60
-# capture on even wallclock times (00:00:00, 00:05:00, 00:10:00)
+# how often (secs) to capture and archive an image (time taken to stack images is accounted for). 0 = one-off capture
+CaptureInterval = 0
+# capture on even wallclock times (12:35:00, 12:40:00, 12:45:00 vs 12:34:56, 12:39:56, 12:44:56)
 WallclockIntervals = False
 ## Stacking ##
-# number of images to stack (law of diminishing return applies)
+# number of images to stack (law of diminishing returns applies)
 StackDepth = 10
 # timespan (secs) between capturing images to be stacked
 StackInterval = 0.25
@@ -38,20 +38,20 @@ import subprocess
 NextCaptureTimestamp = datetime.datetime.min
 
 argParser = argparse.ArgumentParser(description="Yawcam companion script to automatically retrieve, noise-reduce, and archive images. Best results if Yawcam is set to output full-quality JPEGs at maximum resolution.", epilog="IMPORTANT NOTE: ImageStack.exe must be in same directory as yawstack.py")
-argParser.add_argument("--archive", help="Image archive path pattern (default " + ArchivePath.replace("%", "%%") + ")", default=ArchivePath, metavar="PATH")
+argParser.add_argument("--output", help="Image output path pattern (default " + ArchivePath.replace("%", "%%") + ")", default=ArchivePath, metavar="PATH")
 argParser.add_argument("--image-url", help="Webcam image URL (default " + YawcamURL + " aka 'HTTP Output')", default=YawcamURL, metavar="URL")
-argParser.add_argument("--interval", help="Archive interval (seconds, default " + str(CaptureInterval) + ")", default=CaptureInterval, metavar="SECS", type=float)
-argParser.add_argument("--interval-wallclock", help="Capture on even wall-clock times", action="store_true", default=False)
+argParser.add_argument("--interval", help="Archive interval (seconds, default " + str(CaptureInterval) + " = one-off)", default=CaptureInterval, metavar="SECS", type=float)
+argParser.add_argument("--interval-snap", help="Align captures with wall-clock times", action="store_true", default=False)
 argParser.add_argument("--stack-depth", help="Number of images to stack (default " + str(StackDepth) + ")", default=StackDepth, metavar="COUNT", type=int)
 argParser.add_argument("--stack-interval", help="Interval between consecutive images in stack (secs, default " + str(StackInterval) + ")", default=StackInterval, metavar="SECS", type=float)
 argParser.add_argument("--verbose", help="Dump lots of logging to stdout", action="store_true", default=False)
 
 args = argParser.parse_args()
 Verbose = args.verbose
-ArchivePath = args.archive
+ArchivePath = args.output
 YawcamURL = args.image_url
 CaptureInterval = args.interval
-WallclockIntervals = args.interval_wallclock
+WallclockIntervals = args.interval_snap
 StackInterval = args.stack_interval
 StackDepth = args.stack_depth
 
@@ -59,6 +59,21 @@ StackDepth = args.stack_depth
 def verbosePrint(*args):
     if Verbose == True:
         print "".join(str(x) for x in args)
+
+
+def snapToWallclock(timestamp):
+    # python datetime library. meh.
+    year = round((timestamp.year * 365 * 24 * 60 * 60) / CaptureInterval, 0) * CaptureInterval / 365 / 24 / 60 / 60
+    dayOfYear = timestamp.toordinal() - datetime.datetime(timestamp.year, 1, 1).toordinal() + 1
+    dayOfYear = round((dayOfYear * 24 * 60 * 60) / CaptureInterval, 0) * CaptureInterval / 24 / 60 / 60
+    snappedTS = datetime.datetime(int(round(year, 0)), 1, 1)
+    snappedTS = snappedTS + datetime.timedelta(dayOfYear - 1, \
+                                                 round((timestamp.second) / CaptureInterval, 0) * CaptureInterval,\
+                                                 0,\
+                                                 0,\
+                                                 round((timestamp.minute * 60) / CaptureInterval, 0) * CaptureInterval / 60,\
+                                                 round((timestamp.hour * 60 * 60) / CaptureInterval, 0) * CaptureInterval / 60 / 60)
+    return snappedTS
 
 
 def capture():
@@ -72,9 +87,9 @@ def capture():
         if WarnMissedIntersitialInterval and ((datetime.datetime.now()) - lastImageTimestamp).total_seconds() < StackInterval:
             print("WARNING: StackInterval too small - not actually a problem unless you care that pre-stack images are being captured at exact intervals")
 
-        # wait until ts has elapsed
+        # wait until interval has elapsed
         while ((datetime.datetime.now()) - lastImageTimestamp).total_seconds() < StackInterval:
-            time.sleep(0.01)
+            time.sleep(0.33)  # if your webcam updates faster than 30fps, feel free to lower
 
         #capture from yawcam output
         lastImageTimestamp = datetime.datetime.now()
@@ -94,27 +109,40 @@ def capture():
     if not os.path.exists(os.path.dirname(outputPath)):
         os.makedirs(os.path.dirname(outputPath))
 
-    stackParams.append('-save "' + outputPath + '" 90')  # number is jpeg quality
+    stackParams.append('-save "' + outputPath + '" 90')  # magic number is jpeg quality
 
     subprocess.call(" ".join(stackParams), stdout=subprocess.PIPE)
 
-    print "Capture + stack completed at ", str(datetime.datetime.now())
+    print "Capture + stack completed at", datetime.datetime.now()
 
 if WallclockIntervals:
-    print "WIP"
+    if CaptureInterval >= 28 * 24 * 60 * 60:
+        print "WARNING: Wallclock-align behaviour with capture intervals over one month is undefined"
+    NextCaptureTimestamp = snapToWallclock(datetime.datetime.now())
+    if NextCaptureTimestamp < datetime.datetime.now():
+        NextCaptureTimestamp += datetime.timedelta(0, CaptureInterval)
+
 else:
     NextCaptureTimestamp = datetime.datetime.min
 
 while True:
-    if WarnMissedIntersitialInterval and datetime.datetime.now() < NextCaptureTimestamp and NextCaptureTimestamp != datetime.datetime.min:
-        print("WARNING: CaptureInterval too small - not actually a problem unless you care that images are being arcnived at exact intervals")
-    if NextCaptureTimestamp != datetime.datetime.min:
-        print "Next capture will occur in ", round((NextCaptureTimestamp - datetime.datetime.now()).total_seconds()), " secs"
-    # wait for next capture ts to arrive
-    while datetime.datetime.now() < NextCaptureTimestamp:
-        time.sleep(0.1)
-    if WallclockIntervals:
-        print "WIP"
-    else:
+    if CaptureInterval != 0:
+        if WarnMissedIntersitialInterval and datetime.datetime.now() < NextCaptureTimestamp and NextCaptureTimestamp != datetime.datetime.min:
+            print("WARNING: CaptureInterval too small - not actually a problem unless you care that images are being arcnived at exact intervals")
+        if NextCaptureTimestamp != datetime.datetime.min:
+            print "Next capture will occur in", round((NextCaptureTimestamp - datetime.datetime.now()).total_seconds(), 2), "secs at", NextCaptureTimestamp
+
+        # wait for next capture ts to arrive
+        while datetime.datetime.now() < NextCaptureTimestamp:
+            time.sleep(0.33)
+
+        if NextCaptureTimestamp == datetime.datetime.min:
+            NextCaptureTimestamp = datetime.datetime.now()  # set up in the case of non-wallclock timing
         NextCaptureTimestamp = NextCaptureTimestamp + datetime.timedelta(0, CaptureInterval)
+        if WallclockIntervals:
+            NextCaptureTimestamp = snapToWallclock(NextCaptureTimestamp)  # somewhat redundant until that one time the logging runs over a leap second and then BOOM, your archives are ONE SECOND OFF! The horror!
+
     capture()
+
+    if CaptureInterval == 0:
+        break
